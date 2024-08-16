ab5bc3df8266b537c18f6624ca8860a2a4396b2d
AFZAALAHMEDKHAN <afzaalahmedk861@gmail.com>
2024-08-16 12:35:56 +0530

    new change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "region" {
   description = "AWS region"
-  type        = 
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "vprofile-eks"
+}
+
+
+####
+ 
